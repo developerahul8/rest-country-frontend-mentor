98fed9fde7147a3825ffd37c1d031f8e8a2b7378
developerahul8 <developerahul8@gmail.com>
2024-10-30 21:23:11 +0530

    almost complete just searchBar feature is left

--- country.js
+++ country.js
@@ -20,36 +20,149 @@ console.log(countryName);
 backBtn.addEventListener("click",() => {
     history.back();
 })
+// console.log(countryContainer);
+function countryDetailsRender(image,name,native,populationNumber,region,subregion,capitalCity,domain,currencie,language){
+
+// ------------------------------Creating Elements------------------------------------------------------------
+
+    const countryImageContainer = document.createElement("div");
+    const countryImage = document.createElement("img");
+    const countryDetailsContainer = document.createElement("div");
+    const countryName = document.createElement("h1");
+    const countryDetailContainer = document.createElement("div");
+    const leftCountryDetails = document.createElement("div");
+    const nativeName = document.createElement("p");
+    const nativeNameTitle = document.createElement("b");
+    const nativeNameValue = document.createElement("span");
+    const population = document.createElement("p");
+    const populationTitle = document.createElement("b");
+    const populationValue = document.createElement("span");
+    const regionContainer = document.createElement("p");
+    const regionTitle = document.createElement("b");
+    const regionValue = document.createElement("span");
+    const subRegion = document.createElement("p");
+    const subRegionTitle = document.createElement("b");
+    const subRegionValue = document.createElement("span");
+    const capital = document.createElement("p");
+    const capitalTitle = document.createElement("b");
+    const capitalValue = document.createElement("span");
+    const rightCountryDetails = document.createElement("div");
+    const topLevelDomain = document.createElement("p");
+    const topLevelDomainTitle = document.createElement("b");
+    const topLevelDomainValue = document.createElement("span");
+    const currencies = document.createElement("p");
+    const currenciesTitle = document.createElement("b");
+    const currenciesValue = document.createElement("span");
+    const languages = document.createElement("p");
+    const languagesTitle = document.createElement("b");
+    const languagesValue = document.createElement("span");
+    const borderCountriesContainer = document.createElement("div");
+    const borderCountriesTitle = document.createElement("b");
+
+// ------------------------------Classes-----------------------------------------------------------
+
+
+    countryImageContainer.classList.add("countryImage");
+    countryDetailsContainer.classList.add("countryDetails");
+    countryName.classList.add("countryName");
+    countryDetailContainer.classList.add("countryDetail");
+    leftCountryDetails.classList.add("left");
+    leftCountryDetails.classList.add("detailsArea");
+    rightCountryDetails.classList.add("right");
+    rightCountryDetails.classList.add("detailsArea");
+    nativeName.classList.add("nativeName");
+    population.classList.add("population");
+    regionContainer.classList.add("region");
+    subRegion.classList.add("subRegion");
+    capital.classList.add("capital");
+    topLevelDomain.classList.add("topLevelDomain");
+    currencies.classList.add("currencies");
+    languages.classList.add("languages");
+    borderCountriesContainer.classList.add("borderCountries");
+
+// ------------------------------Values------------------------------------------------------------
+
+    countryImage.src = image;
+    countryName.textContent = name;
+    nativeNameTitle.textContent = "Native Name: ";
+    nativeNameValue.textContent = native;
+    populationTitle.textContent = "Population: ";
+    populationValue.textContent = populationNumber;
+    regionTitle.textContent = "Region: ";
+    regionValue.textContent = region;
+    subRegionTitle.textContent = "sub Region: ";
+    subRegionValue.textContent = subregion;
+    capitalTitle.textContent = "Capital: ";
+    capitalValue.textContent = capitalCity;
+    topLevelDomainTitle.textContent = "Top Level Domain: ";
+    topLevelDomainValue.textContent = domain;
+    currenciesTitle.textContent = "Currencies: ";
+    currenciesValue.textContent = currencie;
+    languagesTitle.textContent = "Languages: ";
+    languagesValue.textContent = language;
+    borderCountriesTitle.textContent = "Border Countries: "
+
+// ------------------------------Appending------------------------------------------------------------
+
+    countryContainer.appendChild(countryImageContainer);
+    countryImageContainer.appendChild(countryImage);
+    countryContainer.appendChild(countryDetailsContainer);
+    countryDetailsContainer.appendChild(countryName);
+    countryDetailsContainer.appendChild(countryDetailContainer)
+    countryDetailContainer.appendChild(leftCountryDetails);
+    leftCountryDetails.appendChild(nativeName);
+    nativeName.appendChild(nativeNameTitle);
+    nativeName.appendChild(nativeNameValue);
+    leftCountryDetails.appendChild(population);
+    population.appendChild(populationTitle);
+    population.appendChild(populationValue);
+    leftCountryDetails.appendChild(regionContainer);
+    regionContainer.appendChild(regionTitle);
+    regionContainer.appendChild(regionValue);
+    leftCountryDetails.appendChild(subRegion);
+    subRegion.appendChild(subRegionTitle);
+    subRegion.appendChild(subRegionValue);
+    leftCountryDetails.appendChild(capital);
+    capital.appendChild(capitalTitle);
+    capital.appendChild(capitalValue);
+    countryDetailContainer.appendChild(rightCountryDetails);
+    rightCountryDetails.appendChild(topLevelDomain);
+    topLevelDomain.appendChild(topLevelDomainTitle);
+    topLevelDomain.appendChild(topLevelDomainValue);
+    rightCountryDetails.appendChild(currencies);
+    currencies.appendChild(currenciesTitle);
+    currencies.appendChild(currenciesValue);
+    rightCountryDetails.appendChild(languages);
+    languages.appendChild(languagesTitle);
+    languages.appendChild(languagesValue);
+    countryDetailsContainer.appendChild(borderCountriesContainer);
+    borderCountriesContainer.appendChild(borderCountriesTitle);
+
+}
+
+
 
 fetch(`https://restcountries.com/v3.1/name/${countryName}`)
 .then((res) => res.json())
 .then((data) => {
     data.map((country) => {
         console.log(country);
-        country.borders?.map((border) => {
-            fetch(`https://restcountries.com/v3.1/alpha/${border}`)
-            .then((res) => res.json())
-            .then((data) => {
-                data.map((e) => {
-                    const list = document.createElement("a");
-                    list.textContent = e.name.common
-                    list.href = `country.html?name=${e.name.common}`
-                    borderCountry.appendChild(list)
-                })
-            })
-        })
-        countryImage.src = country.flags.svg;
-        countryNameValue.innerText = country.name.common;
-        nativeName.textContent = Object.values(country.name.nativeName)[0].common;
-        population.textContent = country.population.toLocaleString("en-IN");
-        region.textContent = country.region;
-        subRegion.textContent = country.subregion;
-        capital.textContent = country.capital?.join(", ");
-        topLevelDomain.textContent = country.tld[0];
-        currencies.textContent = Object.values(country.currencies)[0].name;
-        languages.textContent = Object.values(country.languages)
+        const nativeName = Object.values(country.name.nativeName)[0].common;
+        const population = country.population.toLocaleString("en-IN");
+        const region = country.region;
+        const capital = country.capital.join(", ");
+        const subRegion = country.subregion;
+        const topLevelDomain = country.tld[0];
+        const currencies = Object.values(country.currencies)[0].name;
+        const languages = Object.values(country.languages);
+        console.log(languages);
+        const image = country.flags.svg;
+        const name = country.name.common;
+        countryDetailsRender(image,name,nativeName,population,region,subRegion,capital,topLevelDomain,currencies,languages)
     })
 })
+
+
 
 
 
@@ -125,4 +238,6 @@ fetch(`https://restcountries.com/v3.1/name/${countryName}`)
 //             </div>`
 //         })
 //     })
+
+
 
--- script.js
+++ script.js
@@ -1,45 +1,109 @@
 const countryContainer = document.querySelector(".countrySection");
+const searchCountry = document.querySelector("#searchCountry");
+const filterMenu = document.querySelector(".filter");
+const themeMode = document.querySelector(".theme");
 const allCountryAPI = "https://restcountries.com/v3.1/all";
+
+themeMode.addEventListener("click",() => {
+    document.body.classList.toggle("darkTheme");
+    console.log(document.body);
+})
+
+function countryRender(Image,Name,Population,Region,Capital,countryPage){
+    const country = document.createElement("a");
+    const countryImageContainer = document.createElement("div");
+    const countryImage = document.createElement("img");
+    const countryDetails = document.createElement("div");
+    const countryName = document.createElement("h1");
+    const countryPopulationValue = document.createElement("div");
+    const countryPopulationContent = document.createElement("span");
+    const countryPopulationName = document.createElement("b");
+    const countryRegionValue = document.createElement("div");
+    const countryRegionContent = document.createElement("span");
+    const countryRegionName = document.createElement("b");
+    const countryCapitalValue = document.createElement("div");
+    const countryCapitalContent = document.createElement("span");
+    const countryCapitalName = document.createElement("b");
+
+    country.classList.add("countryCard");
+    countryImageContainer.classList.add("countryCardImg");
+    countryDetails.classList.add("countryCardDetails");
+
+
+    country.href = countryPage;
+    countryImage.src = Image;
+    countryName.textContent = Name;
+    countryPopulationContent.textContent = Population;
+    countryRegionContent.textContent = Region;
+    countryCapitalContent.textContent = Capital;
+    countryPopulationName.textContent = "Population: ";
+    countryRegionName.textContent = "Region: ";
+    countryCapitalName.textContent = "Capital: ";
+
+
+    countryContainer.append(country);
+    country.appendChild(countryImageContainer);
+    countryImageContainer.appendChild(countryImage);
+
+    country.append(countryDetails);
+    countryDetails.appendChild(countryName);
+    countryPopulationValue.appendChild(countryPopulationName);
+    countryPopulationValue.appendChild(countryPopulationContent);
+    countryRegionValue.appendChild(countryRegionName);
+    countryRegionValue.appendChild(countryRegionContent);
+    countryCapitalValue.appendChild(countryCapitalName);
+    countryCapitalValue.appendChild(countryCapitalContent);
+    countryDetails.appendChild(countryPopulationValue);
+    countryDetails.appendChild(countryRegionValue);
+    countryDetails.appendChild(countryCapitalValue);
+
+}
 
 fetch(allCountryAPI)
 .then((res) => res.json())
 .then((data) => {
     // console.log(data);
     data.map((el) => {
-        // const item = Object.keys(el.name.nativeName)
         // console.log(el);
-        countryContainer.innerHTML += `<a href="country.html?name=${el.name.common}" class="countryCard">
-                <div class="countryCardImg">
-                    <img src=${el.flags.svg}>
-                </div>
-                <div class="countryCardDetails">
-                    <h1>${el.name.common}</h1>
-                    <div><b>Population: </b>${el.population.toLocaleString('en-IN')}</div>
-                    <div><b>Region: </b>${el.region}</div>
-                    <div><b>Capital: </b>${el.capital?.join(", ")}</div>
-                </div>
-            </a>`
+        const flag = el.flags.svg;
+        const name = el.name.common;
+        const population = el.population.toLocaleString("en-IN")
+        const region = el.region;
+        const capital = el.capital;
+        const countryPage = `country.html?name=${name}`
+
+
+        countryRender(flag,name,population,region,capital,countryPage)
     })
 })
 
-const filterMenu = document.querySelector(".filter");
+
 filterMenu.addEventListener("change",(e) => {
-    countryContainer.innerHTML = ""
+    countryContainer.innerHTML = "";
     fetch(`https://restcountries.com/v3.1/region/${e.target.value}`)
     .then((res) => res.json())
     .then((data) => {
         data.map((filterCountries) => {
-            countryContainer.innerHTML += `<a href="country.html?name=${filterCountries.name.common}" class="countryCard">
-                <div class="countryCardImg">
-                    <img src=${filterCountries.flags.svg}>
-                </div>
-                <div class="countryCardDetails">
-                    <h1>${filterCountries.name.common}</h1>
-                    <div><b>Population: </b>${filterCountries.population.toLocaleString('en-IN')}</div>
-                    <div><b>Region: </b>${filterCountries.region}</div>
-                    <div><b>Capital: </b>${filterCountries.capital?.join(", ")}</div>
-                </div>
-            </a>`
+            console.log(filterCountries);
+            const flag = filterCountries.flags.svg;
+            const name = filterCountries.name.common;
+            const population = filterCountries.population.toLocaleString("en-IN")
+            const region = filterCountries.region;
+            const capital = filterCountries.capital;
+            const countryPage = `country.html?name=${name}`
+
+            countryRender(flag,name,population,region,capital,countryPage)
+        })
+    })
+})
+
+
+searchCountry.addEventListener("input",(e) => {
+    fetch(`https://restcountries.com/v3.1/name/${e.target.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+        data.map((searchedCountry) => {
+
         })
     })
 })
